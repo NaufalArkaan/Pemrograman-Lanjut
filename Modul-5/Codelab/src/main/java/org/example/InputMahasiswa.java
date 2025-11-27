4f7414902b61d541a6d619b8c7b7c81ea34d6021
NaufalArkaan <naufalarkaan25@gmail.com>
2025-11-27 23:49:14 +0700

    update Codelab Module 5

--- Modul-5/Codelab/src/main/java/org/example/InputMahasiswa.java
+++ Modul-5/Codelab/src/main/java/org/example/InputMahasiswa.java
@@ -1,4 +1,4 @@
-package example;
+package org.example;
 
 import java.io.IOException;
 import java.util.Scanner;
--- Modul-5/Codelab/src/main/java/org/example/MahasiswaExcelManager.java
+++ Modul-5/Codelab/src/main/java/org/example/MahasiswaExcelManager.java
@@ -1,4 +1,4 @@
-package example;
+package org.example;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
